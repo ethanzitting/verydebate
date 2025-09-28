4e53fb6515ecf55a7b308b446462096a4ed1609b
Ethan Zitting <ethan@ethanzitting.com>
2025-09-27 21:01:30 -0500

    chore: tweak the audio visualizer

--- app/components/audioVisualizer/tinyAudioVisualizer.tsx
+++ app/components/audioVisualizer/tinyAudioVisualizer.tsx
@@ -1,22 +1,11 @@
-import { useOnMount } from "@/app/components/hooks/useOnMount";
-import { useMicrophoneContext } from "@/app/components/microphone/microphoneContextProvider";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useMicrophoneContext } from '@/app/components/microphone/microphoneContextProvider';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 
-const interpolateColor = (
-  startColor: number[],
-  endColor: number[],
-  factor: number,
-): number[] => {
-  const result = [];
-  for (let i = 0; i < startColor.length; i++) {
-    result[i] = Math.round(
-      startColor[i] + factor * (endColor[i] - startColor[i]),
-    );
-  }
-  return result;
+type Props = {
+  className?: string;
 };
 
-export const AudioVisualizer = () => {
+export const TinyAudioVisualizer: FC<Props> = ({ className = '' }) => {
   const { microphone } = useMicrophoneContext();
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -26,17 +15,18 @@ export const AudioVisualizer = () => {
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
 
   const initializeVisualizer = useCallback(() => {
-    if (typeof window === "undefined") return;
+    const audioContext = new globalThis.AudioContext();
 
-    const audioContext = new (window.AudioContext ||
-      window.webkitAudioContext)();
     const analyser = audioContext.createAnalyser();
+
     const dataArray = new Uint8Array(analyser.frequencyBinCount);
     setAnalyser(analyser);
     setDataArray(dataArray);
     setAudioContext(audioContext);
   }, []);
-  useOnMount(initializeVisualizer);
+  useEffect(() => {
+    initializeVisualizer();
+  }, [initializeVisualizer]);
 
   const draw = useCallback((): void => {
     if (!microphone || !audioContext || !analyser || !dataArray) return;
@@ -56,25 +46,20 @@ export const AudioVisualizer = () => {
 
     requestAnimationFrame(draw);
 
+    // @ts-expect-error slkjdflksjf
     analyser.getByteFrequencyData(dataArray);
 
     if (!context) return;
 
     context.clearRect(0, 0, width, height);
 
-    const barWidth = 10;
+    const barWidth = 4;
     let x = 0;
-    const startColor = [19, 239, 147];
-    const endColor = [20, 154, 251];
 
     for (const value of dataArray) {
       const barHeight = (value / 255) * height * 2;
 
-      const interpolationFactor = value / 255;
-
-      const color = interpolateColor(startColor, endColor, interpolationFactor);
-
-      context.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.1)`;
+      context.fillStyle = `black`;
       context.fillRect(x, height - barHeight, barWidth, barHeight);
       x += barWidth;
     }
@@ -91,5 +76,5 @@ export const AudioVisualizer = () => {
 
   if (!microphone) return null;
 
-  return <canvas ref={canvasRef} width={window.innerWidth}></canvas>;
+  return <canvas ref={canvasRef} width={60} height={60} className={className}></canvas>;
 };
